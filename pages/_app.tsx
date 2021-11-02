842ba9a4172661ac3784d879da3cf12ff7d18f66
Stephen Bonish <sbonish3535@gmail.com>
2021-11-02 16:54:12 -0400

    another try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+
+import dynamic from "next/dynamic";
 
 import Head from 'next/head'
 import Image from 'next/image'
